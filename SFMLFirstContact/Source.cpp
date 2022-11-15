c37d2a72c6b063e44118446e3ce0ef4ab8168e9e
acbsu <100432481@alumnos.uc3m.es>
2022-11-15 14:00:28 +0100

    Resolution parameter

--- SFMLFirstContact/Source.cpp
+++ SFMLFirstContact/Source.cpp
@@ -14,16 +14,17 @@ Walls* boundries;
 
 
 void init(int resMode = 1080, float rayRes = 1) {
+    sf::Vector2f viewSize; 
     switch (resMode)
     {
     case 1080: 
-        sf::Vector2f viewSize(1920, 1080);
+        viewSize = sf::Vector2f(1920, 1080) ;
         break;
     case 800: 
-        sf::Vector2f viewSize(800, 800);
+        viewSize = sf::Vector2f(800, 800);
         break;
     case 720: 
-        sf::Vector2f viewSize(1280, 720);
+        viewSize = sf::Vector2f(1280, 720);
         break;
     default:
         std::cout << "ResMode was wrong, it must be 1080, 800, or 720" << std::endl;
@@ -31,12 +32,11 @@ void init(int resMode = 1080, float rayRes = 1) {
         exit(-1);
         break;
     }
-    sf::Vector2f viewSize(1920, 1080);
     sf::VideoMode vm(viewSize.x, viewSize.y);
     srand(time(NULL));
     boundries = new Walls();
     window = new sf::RenderWindow(vm, "Raycasting 2d demo", sf::Style::Default);
-    particle = new Particle(sf::Vector2f(viewSize.x / 2, viewSize.y / 2));
+    particle = new Particle(sf::Vector2f(viewSize.x / 2, viewSize.y / 2), rayRes);
     
 }
 
@@ -68,15 +68,39 @@ void UpdateInput() {
 
 
 
-int main(int argc, char* argv[]) {
-    if (argc == 2 && argc > 5) {
+int main(int argc, char **argv) {
+    int resMode = 1080; 
+    float raysRes = 1; 
+    
+    if (argc == 2 || argc > 5) 
+    {
         std::cout << "Wrong arguments" << std::endl; 
-        exit(-1); 
+        return(0);
     }
+    if (argc > 2 ) {
+        if (argv[1][0] != '-') 
+        {
+            std::cout << "Wrong argumentes" << std::endl; 
+            return(0); 
+        }
+        for (size_t i = 1; i < 5; i += 2) 
+        {
+            if (argv[i] && !strcmp(argv[i], "-s") && argv[i+1])
+            {
+                if (!strcmp(argv[i+1], "1080")) resMode = 1080;
+                else if (!strcmp(argv[i+1], "720")) resMode = 720;
+                else if (!strcmp(argv[i+1], "800")) resMode = 800;
+                else
+                {
+                    std::cout << "Resolution mode must be: 1080 or 720 or 800" << std::endl; return(0);
+                }
+            }
 
-        
+        }
 
-    init();
+    }
+       
+    init(resMode, raysRes);
     //init game objects 
     while (window->isOpen()) {
         // Handle Keyboard events 
